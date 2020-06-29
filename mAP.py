2a2457390e3b8d67db99cc48cf4af97c60014410
Slimco86 <slimco86@gmail.com>
2020-06-29 09:46:57 +0200

    new mAP

--- mAP.py
+++ mAP.py
@@ -5,14 +5,33 @@ from sklearn.metrics import average_precision_score
 from sklearn.preprocessing import MultiLabelBinarizer
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader 
 
 class Mapper():
-    def __init__(self,true_labels,pred_labels,n_classes=5,threshold=0.5):
-        self.threshold = threshold
-        self.counts = {cls:[] for cls in range(n_classes)}
-        self.true_labels = true_labels
-        self.pred_labels = pred_labels
+    def __init__(self,model_path,data_path,true_labels,pred_labels,n_classes=5):
+        self.threshold = list(np.arange(0,1,0.1))
+        #self.counts = {cls:[] for cls in range(n_classes)}
+        self.counts = {tr:{cls:[] for cls in range(n_classes)} for tr in self.threshold}
+        self.dataset = self.Load_Dataset(data_path)
+        self.model = Load_Model(model_path)
     
+    def Load_Model(self,path):
+        model = torch.load(path)
+        model.train(False)
+        if torch.cuda.is_available():
+            model.cuda()
+        return model
+
+    def Load_Dataset(self,data_path,batch_size=4,num_workers=4):
+        params = {'batch_size':batch_size,
+                  'num_workers':num_workers,
+                  'shuffle':True,
+                  'drop_last':True}
+        dataset = Dataset(root_dir = data_path)
+        data_gen = DataLoader(dataset,params)
+        return data_gen
+
     def IOU(self,bboxes1, bboxes2):
         x11, y11, x12, y12 = np.split(bboxes1, 4, axis=1)
         x21, y21, x22, y22 = np.split(bboxes2, 4, axis=1)
@@ -26,7 +45,7 @@ class Mapper():
         iou = np.round(interArea / (boxAArea + np.transpose(boxBArea) - interArea),3)
         return iou
     
-    def Check_Img(self,ground_truth,preds):
+    def Check_Img(self,ground_truth,preds,threshold):
         true_cls = ground_truth[:,-1] # get list of correct classes
         pred_cls = preds[:,-2] # get list of predicted classes
         pred_conf = preds[:,-1] # get list of prediction confidence
@@ -35,24 +54,33 @@ class Mapper():
 
         for pair in ind:    # for intersection pairs
             if true_cls[pair[0]]==int(pred_cls[pair[1]]):
-                self.counts[true_cls[pair[0]]].append('TP') # True positive if classes match
+                self.counts[threshold][true_cls[pair[0]]].append('TP') # True positive if classes match
             elif true_cls[pair[0]]!=int(pred_cls[pair[1]]):
-                self.counts[true_cls[pair[0]]].append('FN')  # False negative if classes do not match
+                self.counts[threshold][true_cls[pair[0]]].append('FN')  # False negative if classes do not match
         
         pred_boxes = ind[:,0]
         unpr_boxes = np.setdiff1d([i for i in range(ground_truth.shape[0])], pred_boxes) # Get boxes indices for which no bbox was predicted
         for box in unpr_boxes:
-            self.counts[true_cls[box]].append('FP') # set false positive for missed boxes
+            self.counts[threshold][true_cls[box]].append('FP') # set false positive for missed boxes
         return 
 
     def Check_Dataset(self):
-        for ground_truth,preds in tqdm(zip(self.true_labels,self.pred_labels)):
-            self.Check_Img(ground_truth,preds) 
+        
+        progress_bar = tqdm(self.dataset)
+        for iter,sample in progress_bar:
+            if torch.cuda.is_available():
+                preds = self.model(sample['img'].cuda())
+            else:
+                preds = self.model(sample['img'])
+            for treshold in self.threshold:
+                trunc_pred = preds[preds[:,-1]>=threshold]
+                ground_truth = sample['annot']    
+                self.Check_Img(ground_truth,trunc_pred,treshold) 
 
-    def Get_Metrics(self,cls):
-        TP = self.counts[cls].count('TP')
-        FP = self.counts[cls].count('FP')
-        FN = self.counts[cls].count('FN')
+    def Get_Metrics(self,cls,threshold):
+        TP = self.counts[threshold][cls].count('TP')
+        FP = self.counts[threshold][cls].count('FP')
+        FN = self.counts[threshold][cls].count('FN')
         if TP == 0:
             return (0,0,0)
         precision = TP/(TP+FP)
@@ -60,14 +88,14 @@ class Mapper():
         accuracy = TP/len(self.counts[cls])
         return (precision, recall, accuracy)
 
-    def AP(self,cls):
-        TP_tot = self.counts[cls].count('TP')
+    def AP(self,cls,threshold):
+        TP_tot = self.counts[threshold][cls].count('TP')
         TP = 0
         FP = 0
         FN = 0
         precision = [1]
         recall = [0]
-        for q,pred in enumerate(self.counts[cls]):
+        for q,pred in enumerate(self.counts[threshold][cls]):
             
             if pred == 'TP':
                 TP+=1
